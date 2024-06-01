0a85b6ef1da53b70d004152c6a14a48fdead08f7
bowie <axfrontend@gmail.com>
2024-06-01 02:20:13 -0300

    Ajuste
    Commit de ajuste

--- main.js
+++ main.js
@@ -10,14 +10,12 @@ const bot3 = document.querySelectorAll(".bot3")
 
 function animationSetShot(Element,x){
     const randomNumber = Math.floor(Math.random() * 15) + 1;
-    console.log(randomNumber)
     Element[x].style.animation=`Shot${randomNumber}`
     Element[x].style.animationDuration='0.5s'
 }
 
 function animationSetMovie(Element,x){
     const randomNumber = Math.floor(Math.random() * 15) + 1;
-    console.log(randomNumber)
     Element[x].style.animation=`Move${randomNumber}`
     Element[x].style.animationDuration='3.2s'
 }
@@ -28,30 +26,17 @@ for(let x = 0 ; x<4 ; x++){
     var count1 = document.querySelectorAll('.gun')
 
     if(x===0){
-       // console.log('gun1')
         animationSetShot(emmo,x);
-
-        var countt1 = parseInt(count1[0].innerHTML)
-        count1[0].innerHTML=`${countt1+1}`
         
     }else if(x===1){
-        console.log('gun2')
         animationSetShot(emmo,x)
 
-        var countt1 = parseInt(count1[1].innerHTML)
-        count1[1].innerHTML=`${countt1+1}`
     }else if(x===2){
-        console.log('gun3')
         animationSetShot(emmo,x)
-        
-        var countt1 = parseInt(count1[2].innerHTML)
-        count1[2].innerHTML=`${countt1+1}`
+    
     }else if(x===3){
-        console.log('gun4')
         animationSetShot(emmo,x)
-        
-        var countt1 = parseInt(count1[3].innerHTML)
-        count1[3].innerHTML=`${countt1+1}`
+
     }
   })
 }
@@ -73,6 +58,11 @@ setInterval(() => {
     const botPosition2  = bot[1].getBoundingClientRect();
     const botPosition3  = bot[2].getBoundingClientRect();
     const botPosition4  = bot[3].getBoundingClientRect();
+    const botPositionZ5  = bot[4].getBoundingClientRect();
+    const botPositionZ6 = bot[5].getBoundingClientRect();
+    const botPositionZ7  = bot[6].getBoundingClientRect();
+    const botPositionZ8 = bot[7].getBoundingClientRect();
+
     const emmoPosition1 = emmo[0].getBoundingClientRect();
     //------------------------------------------------
     
@@ -95,93 +85,153 @@ setInterval(() => {
     const botPosition16  = bot3[3].getBoundingClientRect();
     const emmoPosition4 = emmo[3].getBoundingClientRect();
     //------------------------------------------------
+
+    var count1 = document.querySelectorAll('.gun')
     
     if(emmoPosition1.bottom<botPosition1.bottom){
         bot[0].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
 
     }else if(emmoPosition1.bottom<botPosition2.bottom){
         bot[1].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition1.bottom<botPosition3.bottom){
         bot[2].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition1.bottom<botPosition4.bottom){
         bot[3].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
+    }else if(emmoPosition1.bottom<botPositionZ5.bottom){
+        bot[4].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
+    }else if(emmoPosition1.bottom<botPositionZ6.bottom){
+        bot[5].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
+    }else if(emmoPosition1.bottom<botPositionZ7.bottom){
+        bot[6].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
+    }else if(emmoPosition1.bottom<botPositionZ8.bottom){
+        bot[7].remove()
+        var countt1 = parseInt(count1[0].innerHTML)
+        count1[0].innerHTML=`${countt1+1}`
     }
 
     //======================================================
     if(emmoPosition2.bottom<botPosition5.bottom){
         bot1[0].remove()
+        var countt1 = parseInt(count1[1].innerHTML)
+        count1[1].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition2.bottom<botPosition6.bottom){
         bot1[1].remove()
+        var countt1 = parseInt(count1[1].innerHTML)
+        count1[1].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition2.bottom<botPosition7.bottom){
         bot1[2].remove()
+        var countt1 = parseInt(count1[1].innerHTML)
+        count1[1].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition2.bottom<botPosition8.bottom){
         bot1[3].remove()
+        var countt1 = parseInt(count1[1].innerHTML)
+        count1[1].innerHTML=`${countt1+1}`
     }
 
     //======================================================
     if(emmoPosition3.bottom<botPosition9.bottom){
         bot2[0].remove()
+        var countt1 = parseInt(count1[2].innerHTML)
+        count1[2].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition3.bottom<botPosition10.bottom){
         bot2[1].remove()
+        var countt1 = parseInt(count1[2].innerHTML)
+        count1[2].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition3.bottom<botPosition11.bottom){
         bot2[2].remove()
+        var countt1 = parseInt(count1[2].innerHTML)
+        count1[2].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition3.bottom<botPosition12.bottom){
         bot2[3].remove()
+        var countt1 = parseInt(count1[2].innerHTML)
+        count1[2].innerHTML=`${countt1+1}`
     }
     //======================================================
     if(emmoPosition4.bottom<botPosition13.bottom){
         bot3[0].remove()
+        var countt1 = parseInt(count1[3].innerHTML)
+        count1[3].innerHTML=`${countt1+1}`
+        
         
     }else if(emmoPosition4.bottom<botPosition14.bottom){
         bot3[1].remove()
+        var countt1 = parseInt(count1[3].innerHTML)
+        count1[3].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition4.bottom<botPosition15.bottom){
         bot3[2].remove()
+        var countt1 = parseInt(count1[3].innerHTML)
+        count1[3].innerHTML=`${countt1+1}`
         
     }else if(emmoPosition4.bottom<botPosition16.bottom){
         bot3[3].remove()
+        var countt1 = parseInt(count1[3].innerHTML)
+        count1[3].innerHTML=`${countt1+1}`
     }
 
 }, 50);
 
 
 
-const BotMovie =()=>{
+const BotMovie =(TimeBot)=>{
 
-    const times = [2000,2003,2006,2009];
-    const times2 = [1000,1003,1006,1009];
-    const times3 = [400,603,806,909];
+    
+    const times = [3000,2900,2800,2000,1000];
+    const times2 = [3000,2900,2800,2000,1000];
+    const times3 = [3000,2900,2800,2000,1000];
 
 setInterval(() => {
-    const randomNumber = Math.floor(Math.random() * 4) + 1;
+    var count1 = document.querySelectorAll('.gun')
+    const randomNumber = Math.floor(Math.random() * 8) + 1;
     animationSetMovie(bot,randomNumber-1)
-    console.log(randomNumber)
-}, 2300);
+    const timeNumber= parseInt(count1[0].innerHTML);
+
+
+}, time[TimeBot] )
+
+
+
 setInterval(() => {
     const randomNumber = Math.floor(Math.random() * 4) + 1;
     animationSetMovie(bot1,randomNumber-1)
-    console.log(randomNumber)
+    
 }, 2000);
 setInterval(() => {
     const randomNumber = Math.floor(Math.random() * 4) + 1;
     animationSetMovie(bot2,randomNumber-1)
-    console.log(randomNumber)
+    
 }, 2800);
 setInterval(() => {
     const randomNumber = Math.floor(Math.random() * 4) + 1;
     animationSetMovie(bot3,randomNumber-1)
-    console.log(randomNumber)
+    
 }, 2600);
 
 }
+
+
 
 BotMovie()
 
